dc40842ad28853f631a692327069aa45351d3c74
mgrumbine <you@example.com>
2022-11-28 09:44:09 -0700

    Currently only failing 3 tests (all in part 4). And will successfully bootrun. Does it function the way it's supposed to? Good question, but this feels like progress.

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -2,21 +2,23 @@ package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Job extends AbstractEntity{
+public class Job extends AbstractEntity {
 
     @ManyToOne
     @NotNull
     private Employer employer;
 
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
 
     public Job() {
     }
 
-    public Job(Employer anEmployer, String someSkills) {
+    public Job(Employer anEmployer, List<Skill> someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -24,18 +26,23 @@ public class Job extends AbstractEntity{
 
     // Getters and setters.
 
-    public Employer getEmployer() { return employer; }
+    public Employer getEmployer() {
+        return employer;
+    }
 
     public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
-        this.skills = skills;
+    public void addSkill(Skill skill) {
+        this.skills.add(skill);
     }
 
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @Size(max = 255)    //Added this because otherwise too long descriptions get white label error. not pretty for users.
     private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs;
 
     public Skill() { //Added this so Hibernate can create an object
     }
@@ -17,5 +22,17 @@ public class Skill extends AbstractEntity {
     }
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void addJob(Job job){
+        this.jobs.add(job);
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
