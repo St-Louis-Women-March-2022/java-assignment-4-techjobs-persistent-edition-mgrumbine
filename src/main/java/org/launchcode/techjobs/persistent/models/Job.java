1533b40d4d8019d1f8b6a88876ef7a5314c7dec8
mgrumbine <you@example.com>
2022-11-29 15:56:27 -0700

    Done for real this time!

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Entity
 public class Job extends AbstractEntity {
